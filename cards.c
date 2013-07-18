de1e02dbcbad676e1f05e666edfc48e0a6c07b29
alistair <alistair@alistair-VirtualBox.(none)>
2013-07-18 22:00:50 +0100

    Basics

--- cards.c
+++ cards.c
@@ -1,6 +1,7 @@
-#include<cards.h>
+#include "cards.h"
 #include<stdlib.h>
 #include<stdio.h>
+#include<time.h>
 
 void display_card(Card *card)
 {
@@ -11,14 +12,43 @@ void display_deck(Deck *deck)
 {
     for (int i = 0; i < 40; i++)
     {
-        display_card(i);
+        display_card(deck->cards[i]);
         printf(" ");
     }
     printf("\n");
 }
 
+int random_range(int min, int max)
+{
+    int r;
+    max = max+1;
+    const int range = max - min;
+    const int buckets = RAND_MAX / range;
+    const int limit = buckets * range;
+
+    /* Create equal size buckets all in a row, then fire randomly towards
+    *      * the buckets until you land in one of them. All buckets are equally
+    *           * likely. If you land off the end of the line of buckets, try again. */
+    do
+    {
+        r = rand();
+    } while (r >= limit);
+
+    return min + (r / buckets);
+
+}
+
 void shuffle_deck(Deck *deck, int inclusive_start, int inclusive_end)
 {
+    int newLocation;
+    Card *tmp;
+    for (int i = inclusive_start; i <= inclusive_end; i++)
+    {
+        newLocation = random_range(i,inclusive_end);
+        tmp = deck->cards[i];
+        deck->cards[i] = deck->cards[newLocation];
+        deck->cards[newLocation] = tmp;
+    }
 }
 
 Card *take_top_card(Deck *deck)
@@ -27,5 +57,32 @@ Card *take_top_card(Deck *deck)
 
 Deck *create_deck()
 {
+    Deck *deck = (Deck *)(malloc(sizeof(Deck)));
+    for (int suit = 0; suit < 4; suit ++)
+    {
+        for (int value = 0; value < 10; value ++)
+        {
+            deck->cards[suit*10+value] = (Card *)(malloc(sizeof(Card)));
+            deck->cards[suit*10+value]->suit = suit; 
+            deck->cards[suit*10+value]->value = value; 
+            deck->cards[suit*10+value]->score = POINTS[value]; 
+        }
+    }
+    return deck;
 }
 
+void main()
+{
+    srand(time(NULL));
+    for (int i = 0; i < 100; i++)
+    {
+        printf("%d,",random_range(0,3));
+    }
+    printf("\n");
+    Deck *deck = create_deck();
+    for (int i = 0; i < 10; i++)
+    {
+        shuffle_deck(deck,0,39);
+        display_deck(deck);
+    }
+}
